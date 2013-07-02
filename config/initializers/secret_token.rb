2383ff2e6fcb5c01770c7e625cf4b46c02014bd0
Michael Hauptman <mike@memeads.com>
2013-07-01 22:31:47 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MemadTest::Application.config.secret_token = '642f57f8e552ad8d3cd67e2915df9ce5662516ed9dc8a306782526bc147e7a58e7f2270a5bf61a018a69c9d9ee1bb46ba9caa9e1ab3a1da094df6d4abd0a533b'
